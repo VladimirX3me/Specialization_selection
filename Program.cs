3116d39b3c4da7066550e24bdbc2b8618bc9177c
Vladimir <vladxtreame@gmail.com>
2022-12-30 11:21:37 +0400

    add new info

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿string [] startingArray = new string[] {"hello", "2", "world", ":-)"};
-string[] resultArray = new string [0];
+﻿string[] startingArray = new string[] { "hello", "2", "world", ":-)" };
+string[] resultArray = new string[0];
+
+foreach (var item in startingArray)
+{
+    if (item.Length <= 3)
+    {
+        Array.Resize(ref resultArray, resultArray.Length + 1);
+        resultArray[resultArray.Length - 1] = item;
+    }
+}
